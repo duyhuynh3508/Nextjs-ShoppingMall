80e9db9b6aec6caac46acf81d9de749b92d6e4f5
Duy <duyhuynh3508@gmail.com>
2025-02-04 10:48:51 +0700

    fix

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -1,17 +1,12 @@
 'use client';
 
 import { signIn } from 'next-auth/react';
-import { useSearchParams } from 'next/navigation';
-
 
 const SignIn = () => {
-  const searchParams = useSearchParams();
-  const error = searchParams.get('error');
 
   return (
     <div>
       <h1>Sign In</h1>
-      {error && <p style={{ color: 'red' }}>Login failed. Please try again.</p>}
       <button onClick={() => signIn('google', { callbackUrl: process.env.NEXTAUTH_URL })}>Sign in with Google</button>
       <button onClick={() => signIn('facebook', { callbackUrl: process.env.NEXTAUTH_URL })}>Sign in with FB</button>
     </div>
