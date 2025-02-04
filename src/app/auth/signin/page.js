3e883d6b534761c5afc816d1b86e18c9e4a0da0c
Duy <duyhuynh3508@gmail.com>
2025-02-04 14:17:19 +0700

    clean code

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -58,14 +58,6 @@ const SignIn = () => {
       </div>
     </div>
   );
-
-  // return (
-  //   <div>
-  //     <h1>Sign In</h1>
-  //     <button onClick={() => signIn("google", { callbackUrl: "/" })}>Sign in with Google</button>
-  //     <button onClick={() => signIn("facebook", { callbackUrl: "/" })}>Sign in with Facebook</button>
-  //   </div>
-  // );
 };
 
 export default SignIn;
--- src/app/page.js
+++ src/app/page.js
@@ -62,20 +62,6 @@ const Home = () => {
       </footer>
     </div>
   );
-
-  // return (
-  //   <div>
-  //     <h1>Welcome to My App</h1>
-  //     {session ? (
-  //       <div>
-  //         <p>Welcome, {session.user.name}</p>
-  //         <button onClick={() => signOut()}>Sign Out</button>
-  //       </div>
-  //     ) : (
-  //       <a href="/auth/signin">Sign In</a>
-  //     )}
-  //   </div>
-  // );
 };
 
 export default Home;
